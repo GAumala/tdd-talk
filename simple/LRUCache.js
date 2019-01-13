edc83454e0f6721ceeda1c5cf883fa545b853d36
Gabriel Aumala <gaumala@protonmail.com>
2019-01-12 20:55:34 -0500

    second pass

--- simple/LRUCache.js
+++ simple/LRUCache.js
@@ -2,10 +2,18 @@ class LRUCache {
   constructor(size) {
     this.size = size;
     this.map = {};
+    this.liveKeys = []
   }
 
   put(key, value) {
     this.map[key] = value;
+
+    this.liveKeys.push(key);
+    if (this.liveKeys.length > this.size) {
+      const keyToEvict = this.liveKeys[0];
+      this.liveKeys.splice(0, 1);
+      delete this.map[keyToEvict]
+    }
   }
 
   get(key) {
