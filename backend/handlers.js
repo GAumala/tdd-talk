4991781b00dd1f542100a8be78fb7c2d1bc7565c
Gabriel Aumala <gaumala@protonmail.com>
2019-01-13 15:38:59 -0500

    express third pass

--- backend/handlers.js
+++ backend/handlers.js
@@ -1,3 +1,5 @@
+const db = require('./db.js');
+
 const insertUser = async body => {
   const { email, name } = body;
   if (!email || !name)
@@ -7,6 +9,14 @@ const insertUser = async body => {
 }
 
 const findUser = async params => {
+  if (typeof params.email !== 'string')
+    return { status: 400, body: 'Bad Request' }
+
+  const [ foundUser ] = await db.findUser(params.email);
+  if (!foundUser) 
+    return { status: 404, body: 'Not Found' };
+
+
 return { status: 500, body: 'Not Implemented Yet' }
 }
 
