f69aa7608895274dc82b3c79d9a1a6450ad3c58a
Gabriel Aumala <gaumala@protonmail.com>
2019-01-12 20:49:00 -0500

    second test

--- simple/LRUCache.test.js
+++ simple/LRUCache.test.js
@@ -8,4 +8,15 @@ describe('LRUCache', () => {
     
     expect(retrieved).toEqual(1);
   })
+  it('discards older entries', () => {
+    const cache = new LRUCache(10);
+    cache.put('first', 1);
+
+    Array(11).fill(0).forEach((item, index) => {
+      cache.put(index, item);
+    })
+
+    expect(cache.get(10)).toBe(0);
+    expect(cache.get(0)).toBeUndefined();
+  })
 })
