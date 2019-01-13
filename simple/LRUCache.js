a7fcfb4a3d223ef31f08a664c515677ab340d08e
Gabriel Aumala <gaumala@protonmail.com>
2019-01-12 23:01:02 -0500

    third pass

--- simple/LRUCache.js
+++ simple/LRUCache.js
@@ -1,18 +1,25 @@
 class LRUCache {
   constructor(size) {
     this.size = size;
+    this.currentSize = 0;
     this.map = {};
     this.liveKeys = []
   }
 
-  put(key, value) {
+  put(key, value, weight) {
     this.map[key] = value;
+    this.currentSize += weight
 
-    this.liveKeys.push(key);
-    if (this.liveKeys.length > this.size) {
-      const keyToEvict = this.liveKeys[0];
+    this.liveKeys.push({ key, weight });
+    if (this.currentSize > this.size) {
+      const { 
+        key: keyToEvict, 
+        weight: evictedWeight 
+      } = this.liveKeys[0];
+
       this.liveKeys.splice(0, 1);
       delete this.map[keyToEvict]
+      this.currentSize -= evictedWeight
     }
   }
 
